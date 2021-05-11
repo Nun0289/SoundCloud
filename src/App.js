809a52c3f29f5ded9e021e82444f9f8181527fc2
Champchips <otakuchamp@gmail.com>
2021-05-11 15:50:53 +0700

    add ApolloProvider

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import "./App.css";
 import SideNavbar from "./component/Navbar/Navbar_Side";
 import TopNavbar from "./component/Navbar/Navbar_top";
 import Footer from "./component/Footer";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Home from "./page/Home";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <TopNavbar />
-        <SideNavbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-        </Switch>
-        <Footer />
-      </Router>
+      <TopNavbar />
+      <SideNavbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+      <Footer />
     </div>
   );
 }
